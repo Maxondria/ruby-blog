8fb8f1e5668bab24bc167ef8b84c32b19f250688
Maxon Tayebwa <maxtayebwa@gmail.com>
2022-05-26 17:27:19 +0300

    annotate models

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id            :bigint           not null, primary key
+#  user_id       :bigint           not null
+#  likeable_id   :bigint           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  likeable_type :string
+#
 class Like < ApplicationRecord
   validates :user_id, uniqueness: { scope: %i[likeable_id likeable_type] }
 
